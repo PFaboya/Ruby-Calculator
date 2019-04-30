800e3c6dcea3c2cc86a8d29dd4b7be8d3a564b75
Tech-A08 <tech-a08@MacBook-Air-3.local>
2019-04-30 16:56:59 +0100

    bmi calculator done

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 
-puts "Please type in b for the basic calculator, a for the advanced calculator"
+puts "Please type in b for the basic calculator, a for the advanced calculator and bmi for bmi calculator"
 type = gets.chomp
 
 if type == "b"
@@ -11,8 +11,8 @@ if type == "b"
   operation = gets.chomp
   puts "Please Select your second number"
   secondNumber = gets.chomp
-  # secondNumberInt = eval secondNumber
   puts "Answer #{eval firstNumberString + operation + secondNumber}"
+
 elsif type == "a"
   puts "Adavanced Calculator selected, Please choose your first number"
   firstNumber = gets.chomp
@@ -25,5 +25,27 @@ elsif type == "a"
   elsif operation == "sqrt"
     firstNumberFloat = firstNumber.to_f
     puts "Answer #{Math.sqrt(firstNumberFloat)}"
+  end
+
+elsif type == "bmi"
+  puts "select measurement system, i for imperial and m for metric"
+  measurement = gets.chomp
+  if measurement == "m"
+    puts "Please select your Height in metres"
+    height = gets.chomp
+    heightInt = height.to_f
+    puts "Please select your weight in kilograms"
+    mass = gets.chomp
+    massFloat = mass.to_f
+    puts "Answer #{massFloat / (heightInt ** 2)}"
+  end
+  if measurement == "i"
+    puts "Please select your Height in inches"
+    height = gets.chomp
+    heightInt = height.to_f
+    puts "Please select your weight in Pounds"
+    mass = gets.chomp
+    massFloat = mass.to_f
+    puts "Answer #{(massFloat / (heightInt ** 2))*703}"
   end
 end
